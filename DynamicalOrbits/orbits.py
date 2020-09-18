45fc0f5745cc2f30d912d2781a5c5ec68c9c06e8
Gonzalo <goznalomail@gmail.com>
2020-09-18 12:17:43 +0200

    Orbits figures (trajectory gif + phasespace/potential)

--- DynamicalOrbits/orbits.py
+++ DynamicalOrbits/orbits.py
@@ -41,29 +41,52 @@ statetheta = odeint(equation_theta, statei_theta, t)
 xtot = np.multiply(stater[:,0], np.cos(statetheta[:,0]))
 ytot = np.multiply(stater[:,0], np.sin(statetheta[:,0]))
 
+
+#figure parameters (height, width...)
+def set_size(w,h, ax=None):
+    """ w, h: width, height in inches """
+    if not ax: ax=plt.gca()
+    l = ax.figure.subplotpars.left
+    r = ax.figure.subplotpars.right
+    t = ax.figure.subplotpars.top
+    b = ax.figure.subplotpars.bottom
+    figw = float(w)/(r-l)
+    figh = float(h)/(t-b)
+    ax.figure.set_size_inches(figw, figh)
+
 # plotting the trajectory
-plt.style.use('fivethirtyeight')
+plt.style.use('seaborn')
 
 fig, ax = plt.subplots()
-fig2, (ax1,ax2) = plt.subplots(ncols = 1, nrows = 2)
+fig2, (ax1,ax2) = plt.subplots(ncols = 2, nrows = 1)
 
-ax.set_title('Orbital motion (m=2)')
+fig.suptitle('Orbital motion(m = 2)')
+
 ax1.set_title('Radial phase space')
-ax2.set_title('Potential')
+ax2.set_title('Central potential')
 #ax3.set_title('Angular phase space')
 
+#Radial phase space
+ax1.plot(stater[:,0], m * stater[:,1], linewidth = 2, label = "m = 2")
+ax1.legend(loc = "best")
+ax1.set_xlabel('r')
+ax1.set_ylabel('m dr/dt')
+
+#Potential vs radius
 rvals = np.linspace(0.01,25,1000)
 ax2.plot(rvals, 1/np.power(rvals,3) - 1/rvals, label =  "V(r)")
 ax2.legend(loc = "best")
+ax2.set_xlabel('r')
+ax2.set_ylabel('V')
 
 #ax3.plot(statetheta[:,0],statetheta[:,1], linewidth = 3, label = "m = 2") #not really interesting.
-
 ax2.set_xlim([-1, 20])
 ax2.set_ylim([-1, 5])
 
+set_size(7,3)
+
 def draw_orbit(i):
     ax.cla()
-    ax1.cla()
     #ax.set_aspect('equal', adjustable='box')
 
     rnow = stater[i,0]
@@ -82,11 +105,18 @@ def draw_orbit(i):
     ax.legend(loc = 'best')
 
     ########### Radial phase space
-    ax1.plot(stater[:,0],stater[:,1], linewidth = 3, label = "m = 2")
-    ax1.plot(rnow,rdotnow, 'or')
-    ax1.legend(loc = "best")
+    #ax1.plot(stater[:,0],stater[:,1], linewidth = 3, label = "m = 2")
+    #ax1.plot(rnow,rdotnow, 'or')
+    #ax1.legend(loc = "best")
 
-ani = FuncAnimation(fig, draw_orbit, frames  = 400, interval = 10, repeat = True)
+frames = 600
+ani = FuncAnimation(fig, draw_orbit, frames  = frames, interval = 10, repeat = True)
+
+figfile = "./DynamicalOrbits/PhaseSpace-Potential.png"
+fig2.savefig(figfile, facecolor='w', edgecolor='w',format='png')
+
+anifile = f"./DynamicalOrbits/CentralForceOrbits-{frames}.gif"
+ani.save(anifile, writer='imagemagick')
 
 plt.show()
 
